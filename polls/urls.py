9032ef7a80e31ea2fd985bb45abe50b0055ad92c
Ionek Dev <ionekbd16@gmail.com>
2019-05-11 12:57:51 +0600

    pulls update

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail')
 ]
