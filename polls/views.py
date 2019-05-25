0bc0619d56de8f33de54e93a25d83c1c40ac46d4
Ionek Dev <ionekbd16@gmail.com>
2019-05-25 14:31:21 +0600

    Initial commit

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # (1)
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+    # (2)
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    # (3)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s. " % question_id)
+    # (1)
+    # return HttpResponse("You're looking at question %s. " % question_id)
+
+    # (2)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist, lol")
+    # return render(request, 'polls/detail.html', {'question':question})
+
+    # (3)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
-def result(request, question_id):
+def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
